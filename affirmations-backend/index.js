004adf816dfbf1cf977be68f1f410ef40171d8b3
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-06-21 14:09:37 -0400

    added dummy affirmations data, route to get the data in json format

--- affirmations-backend/index.js
+++ affirmations-backend/index.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const app = express()
 
+const affirmations = [
+    {
+        "userID": 1,
+        "affirmationID": 1,
+        "setID": 1,
+        "content": "An eye for an eye makes the whole world blind.",
+        "author": "Ghandi",
+        "style": "default"
+    },
+    {
+        "userID": 1,
+        "affirmationID": 2,
+        "setID": 1,
+        "content": "Love is love.",
+        "style": "default"
+    },
+    {
+        "userID": 1,
+        "affirmationID": 3,
+        "setID": 2,
+        "content": "The happiness of your life depends upon the quality of your thoughts.",
+        "author": "Marcus Aurelius",
+        "style": "default"
+    }
+]
+
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.json(affirmations)
 })
 
 const PORT = 3001
 app.listen(PORT)
+console.log('starting server at port' + PORT)
