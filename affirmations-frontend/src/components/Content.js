6eeb583986196f7102af9fc93e64b1d23bc52aac
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-06-30 13:44:44 -0400

    ui component of adding a new set

--- affirmations-frontend/src/components/Content.js
+++ affirmations-frontend/src/components/Content.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import dataAccess from '../services/dataAccess'
+import plus from '../images/plus.svg'
 
 function Content(props) {
     const [sets, setSets] = useState([])
@@ -12,11 +13,26 @@ function Content(props) {
     }, [])
 
     return (
-        <div class="sets-flex">
-        {
-            sets.map((set) =>
-                <div class="set-item">{set.setID} {set.Name} {set.Description} {set.Quantity} {set.dateUpdated} </div>)
-        }
+        <div className="sets-flex">
+            <div className="set-item" >
+                <div className="set-name">
+                    <img src={plus}/>
+                </div>
+            </div>
+            {
+                sets.map((set) =>
+                    <div className="set-item">
+                        <div className="set-name">
+                            {set.Name}
+                        </div>
+                        <div className="set-quantity">
+                            {set.Quantity}
+                        </div>
+                        <div className="set-date">
+                            {new Date(set.dateUpdated).toDateString()}
+                        </div>
+                    </div>)
+            }
         </div>
     );
 }
